7a22e54d4a48af4283740320097e0c735c96501c
magiqfoxx <katarzyna.zemsta@vp.pl>
2020-10-23 18:21:15 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const messages = [
+    "no chyba nie",
+    "no chyba ty",
+    "tak było, nie zmyślam",
+    "no to ŚWIETNIE!",
+    "AHA.",
+  ];
+  const [text, setText] = useState("");
+  const [shaking, setShaking] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>
+        Magiczna kula pomoze Ci podjąć decyzję. Po prostu zadaj pytanie i
+        potrząśnij.
+      </h2>
+      <button
+        onClick={() => {
+          setText(messages[Math.floor(Math.random() * 4)]);
+          setShaking(true);
+          setTimeout(() => {
+            setShaking(false);
+          }, 4000);
+        }}
+      >
+        shake
+      </button>
+      <div className={shaking ? "ball shaking" : "ball"}>
+        <div className={shaking ? "shaking-message message" : "message"}>
+          <span className={shaking ? "shaking-text text" : "text"}>{text}</span>
+        </div>
+      </div>
     </div>
   );
 }
